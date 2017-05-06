5f99dc85347d012bc19f207cb6f018ce31b189e6
Matshipa <barnad.matshipa1@wipro.com>
2017-05-06 20:27:39 +0200

    First Try

---
+++ src/test/java/com/wipro/bank/entity/BankProcessTest.java
@@ -0,0 +1,19 @@
+package com.wipro.bank.entity;
+
+import junit.framework.TestCase;
+
+public class BankProcessTest extends TestCase {
+
+	public void testBankProcess() {
+		//fail("Not yet implemented");
+	}
+
+	public void testTransferFunds() {
+		//fail("Not yet implemented");
+	}
+
+	public void testGetBalanceOf() {
+		//fail("Not yet implemented");
+	}
+
+}
